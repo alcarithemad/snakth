dedfc9442708a5d87aee4156eb1feca45daa56ed
Colin Valliant <alcarithemad@gmail.com>
2013-04-21 04:04:19 -0600

    Don't lex twice

--- lisp.py
+++ lisp.py
@@ -5,16 +5,13 @@ from parser import Parser
 if __name__ == '__main__':
     from pprint import pprint
     import sys
-    #ex = '(raw_input (str (** (~ (and 2 3)) (int "101" (+ 1 1)))))'
     ex = file(sys.argv[1], 'rb').read()
-    # ex = '(if False (print))'
-    # ex = '(print (> 3 2))'
-    l = Lexer(ex)
-    while 1:
-        x = l.token()
-        # pprint(x)
-        if x == l.EOF:
-            break
+    # l = Lexer(ex)
+    # while 1:
+    #     x = l.token()
+    #     # pprint(x)
+    #     if x == l.EOF:
+    #         break
     l = Lexer(ex)
     p = Parser(l)
     p.run()
