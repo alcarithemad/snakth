1b291c86431c283bcae4ced443b74255fcfd18f3
Colin Valliant <alcarithemad@gmail.com>
2013-04-21 02:52:42 -0600

    Add some tests

---
+++ func-test.lisp
@@ -0,0 +1,2 @@
+(def foo (a b:1 *c **d) (print a b c d))
+(foo 0)
---
+++ test-interactive.lisp
@@ -0,0 +1,3 @@
+(if (>= 3 (int (raw_input "3 >= x? ")))
+        (print (str object: (and True True False)) "asdf\"" nl:False)
+        (print "test"))
